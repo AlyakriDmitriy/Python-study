ba161503aec3257783a8e245617b1120a37ef5fa
AlyakriDmitriy <123932697+AlyakriDmitriy@users.noreply.github.com>
2023-03-29 18:31:12 +0700

    Update crop_images.py

--- crop_images.py
+++ crop_images.py
@@ -6,7 +6,7 @@ import random
 dir_from = "C:/practice/"
 dir_to = "C:/saveimages/"
 
-def Crop_image(filename, new_file_direc, block_size):
+def crop_image(filename: flt, new_file_direc: flt, block_size: int):
     #the first parameter of the function is directory of the file + filename (string)
     #the second parameter of the function is final directory (string)
     #the third parameter is what is the size of blocks in pixel to you want to divide the image (int)
